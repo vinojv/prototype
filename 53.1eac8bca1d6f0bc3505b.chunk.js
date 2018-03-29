bb9a7b58c15c8c90a1963f656636fe1baba48991
Vinoj <vinoj@razorthink.com>
2018-03-29 21:24:08 +0530

    try 2

---
+++ 53.1eac8bca1d6f0bc3505b.chunk.js
@@ -0,0 +1,11 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[53],{
+
+/***/ "../EVA Design System/node_modules/brace/snippets/velocity.js":
+/***/ (function(module, exports) {
+
+eval("ace.define(\"ace/snippets/velocity\",[\"require\",\"exports\",\"module\"],function(e,t,n){\"use strict\";t.snippetText='# macro\\nsnippet #macro\\n\t#macro ( ${1:macroName} ${2:\\\\$var1, [\\\\$var2, ...]} )\\n\t\t${3:## macro code}\\n\t#end\\n# foreach\\nsnippet #foreach\\n\t#foreach ( ${1:\\\\$item} in ${2:\\\\$collection} )\\n\t\t${3:## foreach code}\\n\t#end\\n# if\\nsnippet #if\\n\t#if ( ${1:true} )\\n\t\t${0}\\n\t#end\\n# if ... else\\nsnippet #ife\\n\t#if ( ${1:true} )\\n\t\t${2}\\n\t#else\\n\t\t${0}\\n\t#end\\n#import\\nsnippet #import\\n\t#import ( \"${1:path/to/velocity/format}\" )\\n# set\\nsnippet #set\\n\t#set ( $${1:var} = ${0} )\\n',t.scope=\"velocity\",t.includeScopes=[\"html\",\"javascript\",\"css\"]})\n\n//# sourceURL=webpack:///../EVA_Design_System/node_modules/brace/snippets/velocity.js?");
+
+/***/ })
+
+}]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiI1My4xZWFjOGJjYTFkNmYwYmMzNTA1Yi5jaHVuay5qcyIsInNvdXJjZVJvb3QiOiIifQ==
